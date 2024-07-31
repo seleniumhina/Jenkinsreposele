3215ff186627e37b0d89f301f30c39e83498f92e
hinai <hinainamdar678@gmail.com>
2024-07-31 18:50:51 +0530

    no check

--- src/test/java/repopack/classrepo.java
+++ src/test/java/repopack/classrepo.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+
 
 public class classrepo {
 	 private WebDriver driver;
